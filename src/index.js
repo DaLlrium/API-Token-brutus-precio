8bbc18b869b733c0fb77b1bcfb1875a7d4c59c76
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-06-03 11:52:49 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,31 @@ const bodyParser = require("body-parser");
 const moment = require('moment-timezone');
 const fetch = require('node-fetch');
 
+const TronWeb = require('tronweb');
+
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const app = express();
 const port = process.env.PORT || 3004;
+const PEKEY = process.env.APP_PRIVATEKEY;
+
+const TRONGRID_API = "https://api.trongrid.io";
+const addressContract = "TBRVNF2YCJYGREKuPKaP7jYYP9R1jvVQeq";
+
+tronWeb = new TronWeb(
+	TRONGRID_API,
+	TRONGRID_API,
+	TRONGRID_API,
+	PEKEY
+);
+
 
 
 app.get('/api/v1',async(req,res) => {
 
     res.send("Conectado y funcionando v1.0");
 });
+
 
 app.get('/api/v1/precio/:moneda',async(req,res) => {
 
@@ -28,7 +43,17 @@ app.get('/api/v1/precio/:moneda',async(req,res) => {
 
   	var response = {};
 
+	let contract = await tronWeb.contract().at(addressContract);
+	let RATE = await contract.RATE().call();
+	RATE = parseInt(RATE._hex);
+
+	if(RATE != precio*10**6){
+		await contract.ChangeRate(precio*10**6).send();
+	}
+
 	if (moneda == "BRUT" || moneda == "brut" || moneda == "brut_usd" || moneda == "BRUT_USD") {
+		
+	
 		response = {
 				"Ok": true,
 		    	"Message": "",
